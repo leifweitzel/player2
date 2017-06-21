91c541bfe864caa3ab7031fad73a946d1f2f0db4
Leif Weitzel <leif@leifs-mbp.fritz.box>
2017-06-21 12:24:45 +0200

    Initial Commit

---
+++ player2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  player2
+//
+//  Created by Leif Weitzel on 21.06.17.
+//  Copyright © 2017 Leif Weitzel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
