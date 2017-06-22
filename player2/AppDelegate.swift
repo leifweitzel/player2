6f23691a86a3797ccde17635e8a0b7a05ff60549
Leif Weitzel <leif@eduroam-058-092.scc.uni-weimar.de>
2017-06-22 12:04:37 +0200

    Alles fein

--- player2/AppDelegate.swift
+++ player2/AppDelegate.swift
@@ -7,17 +7,23 @@
 //
 
 import UIKit
+import CoreLocation
+
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
+    var locationManager: CLLocationManager?
 
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        locationManager = CLLocationManager()
+        locationManager?.requestWhenInUseAuthorization()
         return true
     }
+
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
--- player2/ViewController.swift
+++ player2/ViewController.swift
@@ -7,19 +7,111 @@
 //
 
 import UIKit
+import AVFoundation
+import AVKit
+import MapKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, MKMapViewDelegate {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    let avPlayerViewController = AVPlayerViewController()
+    var avPlayer:AVPlayer?
+    
+    @IBOutlet weak var sprachSwitch: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //background audio
+        do {
+            try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback)
+            print("AVAudioSession Category Playback OK")
+            do {
+                try AVAudioSession.sharedInstance().setActive(true)
+                print("AVAudioSession is Active")
+            } catch let error as NSError {
+                print(error.localizedDescription)
+            }
+        } catch let error as NSError {
+            print(error.localizedDescription)
+        }
+        //done
+
+        
+        mapView.delegate = self
+        
+        let annotation = MKPointAnnotation()
+        
+        annotation.coordinate = CLLocationCoordinate2D(latitude: 51.0215077, longitude: 11.2492387)
+        
+        mapView.addAnnotation(annotation)
+        mapView.showsUserLocation = true
+
+        
+        
+        
+        
+        func zoomIn(_ sender: AnyObject) {
+            let userLocation = mapView.userLocation
+            
+            let region = MKCoordinateRegionMakeWithDistance(
+                userLocation.location!.coordinate, 2000, 2000)
+            
+            mapView.setRegion(region, animated: true)
+        }
+
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+ /*
+    @IBAction func languageSwitched(_ sender: Any) {
+        
+        switch(sprachSwitch.selectedSegmentIndex) {
+        case 0:
+            print("fisrt")
+        case 1:
+            print("second")
+        default:
+            break
+        }
     }
+        
+*/
+    
+    @IBAction func playButtonTapped(_ sender: UIButton) {
+        print("Song+\(sprachSwitch.selectedSegmentIndex)")
+        let urlPathString:String? = Bundle.main.path(forResource: "Song\(sprachSwitch.selectedSegmentIndex)", ofType: "mp3")
 
-
+        if let urlPath = urlPathString
+        {
+            let movieUrl = NSURL(fileURLWithPath: urlPath)
+            
+            self.avPlayer = AVPlayer(url: movieUrl as URL)
+            self.avPlayerViewController.player = self.avPlayer
+            
+            
+        }
+        
+        self.present(avPlayerViewController, animated: true) {
+            self.avPlayerViewController.player!.play()
+        }
+    }
+    
+    func mapView(_ mapView: MKMapView, didUpdate
+        userLocation: MKUserLocation) {
+        mapView.centerCoordinate = userLocation.location!.coordinate
+    }
+    
+    @IBAction func zoomIn(_ sender: AnyObject) {
+        let userLocation = mapView.userLocation
+        
+        let region = MKCoordinateRegionMakeWithDistance(
+            userLocation.location!.coordinate, 2000, 2000)
+        
+        mapView.setRegion(region, animated: true)
+    }
+    
 }
 
